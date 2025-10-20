c9994c6f307eeeef2d9f0f89a38df885b6e659b2
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2025-10-20 18:04:16 +0300

    new change

---
+++ React/third-party/v19/react/jsx-dev-runtime/react-jsx-dev-runtime.js
@@ -0,0 +1,10 @@
+define("react/jsx-dev-runtime", ['exports'], (function (exports) { 'use strict';
+
+  const REACT_FRAGMENT_TYPE = Symbol.for('react.fragment');
+
+  const jsxDEV = undefined;
+
+  exports.Fragment = REACT_FRAGMENT_TYPE;
+  exports.jsxDEV = jsxDEV;
+
+}));
