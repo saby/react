3d9ca808c1355738a0923dc51eaedfe2a7229297
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-15 18:06:57 +0300

    new change

---
+++ build-react/v17/constFixesMap.js
@@ -0,0 +1,5 @@
+// Map of search/replace patches to apply to specific files (optional for v17)
+module.exports = {
+  // example:
+  // "react/react.js": [{ search: "__DEV__", replace: "false" }],
+};
